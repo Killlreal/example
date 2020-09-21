730b226d40ac967598c9c5a1cf1a46e0cb074dae
Killlreal <sopochkin.kirill@yandex.ru>
2020-09-22 01:01:11 +0300

    second page

---
+++ src/scripts/script.js
@@ -0,0 +1,9 @@
+$(document).on("DOMContentLoaded", () => {
+  $(".js-slider").slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  });
+});
