534e01f906b80bf798d5b992138350f07c7680a0
Killlreal <sopochkin.kirill@yandex.ru>
2020-09-28 08:52:59 +0300

    sgmu

--- build/scripts/script.js
+++ build/scripts/script.js
@@ -1,20 +1,81 @@
-let button = document.querySelector(".availabel__write-button");
-let blocks = document.querySelector(".blocks");
+
+//TODO: http://prgssr.ru/development/tehniki-raboty-s-dom-roditelskie-dochernie-i-sosednie-elementy.html#heading-section
+// https://learn.javascript.ru/event-delegation
+// https://tproger.ru/translations/dom-javascript/
 (function () {
-  button.onclick = function () {
-    blocks.classList.toggle("hidden");
+  let button = document.querySelectorAll(".availabel__write-button");
+  let blocks = document.querySelectorAll(".blocks");
+
+  for (let j = 0; j < blocks.length; j++) {
+    for (let i = 0; i < button.length; i++) {
+      button[i].onclick = function () {
+        blocks[i].classList.toggle("hidden");
+      };
+    }
+  }
+})();
+
+
+(function () {
+  let parent = document.querySelector('.editor__files')
+  let buttondelete = document.querySelectorAll(".editor__delete-button");
+  let formdelete = document.querySelectorAll(".editor__form-block");
+
+  for (let j = 0; j < formdelete.length; j++) {
+    for (let i = 0; i < buttondelete.length; i++) {
+      buttondelete[i].onclick = function () {
+        console.log(buttondelete[i].innerHTML);
+        buttondelete[i].style.color = 'red';
+        formdelete[i].parentNode.removeChild(formdelete[i]);
+      }
+    }
+  }
+})();
+
+
+
+(function () {
+  let textarea = document.querySelectorAll(".textarea-input");
+
+  for (let i = 0; i < textarea.length; i++) {
+    textarea[i].addEventListener("keyup", function () {
+      if (this.scrollTop > 0) {
+        this.style.height = this.scrollHeight + "px";
+      }
+    });
+  }
+})();
+
+(function () {
+  let titlehidden = document.querySelector('.editor__title');
+  let blockform = document.querySelectorAll('.delete-form');
+  let buttoneditor = document.querySelectorAll('.editor__rewrite-button');
+
+  for (let j = 0; j < blockform.length; j++) {
+    for (let i = 0; i < buttoneditor.length; i++) {
+      buttoneditor[i].onclick = function () {
+        blockform[i].classList.toggle('delete');
+        if (!blockform[i].classList.contains('delete')) {
+          titlehidden.classList.remove('delete')
+        } else {
+          titlehidden.classList.add('delete')
+        }
+      }
+    }
   };
 })();
 
-let textarea = document.querySelector(".textarea-input");
 
-(function () {
-  textarea.addEventListener("keyup", function () {
-    if (this.scrollTop > 0) {
-      this.style.height = this.scrollHeight + "px";
-    }
-  });
-})();
+
+
+//buttoneditor.onclick = function () {
+//  console.log('fsfdsfsddsfdsf')
+//  blockform.classList.toggle('delete');
+//  titlehidden.classList.toggle('delete');
+//}
+
+
+//TODO: взял с сайта, отправку формы на сервер можно delete
 
 document.addEventListener("DOMContentLoaded", () => {
   const forms = document.querySelectorAll("form");
